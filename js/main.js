00ccc85db300c8cf6c973d58775e06ec1aa97229
jolan <mc-somango@live.fr>
2017-07-25 22:13:12 +0200

    progress bar

--- js/main.js
+++ js/main.js
@@ -4,4 +4,19 @@ $(function() {
     // Initialize collapsible (uncomment the line below if you use the dropdown variation)
     //$('.collapsible').collapsible();
     $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+    $('.skill-icons-prog').children('.active').each(function(i) {
+        var row = $(this);
+        row.css('background','#0082c8');
+    });
+
+    $('.skill-icons-web').children('.active').each(function(i) {
+        var row = $(this);
+        row.css('background','#45a247');
+    });
+
+    $('.skill-icons-tools').children('.active').each(function(i) {
+        var row = $(this);
+        row.css('background','#c0392b');
+    });
 });
